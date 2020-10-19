26afb46c1e0371b50c0d62b2793ccb9a56229fa9
Waisath-CJ <curt.waisath.jr@outlook.com>
2020-10-19 12:42:42 -0700

    Add Check Winner Function
    Added function for checking for a winner. It doesn't work currently, so it's commented out for now.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -3,6 +3,7 @@ const ui = require('./ui')
 const store = require('../store')
 
 let currentPlayer = '✕'
+let gameOver = false
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -14,14 +15,41 @@ const winningConditions = [
   [2, 4, 6]
 ]
 
-const checkWinner = () => {
-  return false
-}
+// Got some of the following code for determining a winner from the following website:
+// https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn
+// const checkWinner = () => {
+//   let roundWon = false
+//   for (let i = 0; i <= 7; i++) {
+//     const winCondition = winningConditions[i]
+//     let a = store.game[winCondition[0]]
+//     let b = store.game[winCondition[1]]
+//     let c = store.game[winCondition[2]]
+//     if (a === '' || b === '' || c === '') {
+//       continue
+//     }
+//     if (a === b && b === c) {
+//       roundWon = true
+//       break
+//     }
+//   }
+//   if (roundWon) {
+//     return 'win'
+//   }
+//   /* 
+//   We will check weather there are any values in our game state array 
+//   that are still not populated with a player sign
+//   */
+//   let roundDraw = !store.game.cells.includes("")
+//   if (roundDraw) {
+//     return 'draw'
+//   }
+// }
 
 const onCreateGame = e => {
   e.preventDefault()
 
   if (currentPlayer === 'O') currentPlayer = '✕'
+  if (gameOver === true) gameOver = false
 
   api.createGame()
     .then(ui.createGameSuccess)
@@ -32,8 +60,18 @@ const onBoxClick = e => {
   e.preventDefault()
   const box = $(e.target)
 
-  if (!box.text()) {
-    api.updateGame(box.data('cellIndex'), currentPlayer, checkWinner())
+  if (!box.text() && !gameOver) {
+    // if (checkWinner()) {
+    //   console.log(currentPlayer, ' wins!')
+    //   gameOver = true
+    // } else if (!store.game.cells.includes('')) {
+    //   console.log('It\'s a draw!')
+    //   gameOver = true
+    // }
+
+    // if (checkWinner() === 'win')
+
+    api.updateGame(box.data('cellIndex'), currentPlayer, gameOver)
       .then(ui.updateGameSuccess)
       .then(() => currentPlayer = currentPlayer === 'O' ? '✕' : 'O')
       .catch(ui.updateGameFailure)
